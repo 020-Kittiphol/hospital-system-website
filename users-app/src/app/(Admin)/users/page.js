2cdcdf4bf97b31e2698149f13ebc4e37fe8aba3d
6710210916pimmada <6710210916@psu.ac.th>
2026-04-01 15:13:33 +0700

    Update page.js

--- users-app/src/app/(Admin)/users/page.js
+++ users-app/src/app/(Admin)/users/page.js
@@ -1,14 +1,13 @@
 "use client";
 
 import React from 'react';
-import RegisterForm from './components/users_form'; // เช็ค Path ให้ตรงกับที่เซฟไว้นะครับ
-import './register.css'; // อย่าลืม Import ไฟล์ CSS นะครับ
+import RegisterForm from './components/users_form';
+import './register.css';
 
 export default function RegisterPage() {
     return (
         <div className="reg-wrapper">
-            
-            {/* ดึงกล่องการ์ดมาแสดงตรงนี้ */}
+        
             <RegisterForm />
         </div>
     );
