a58a0d68f3d48890d2f5a12049233bdc9c53267f
aekkarin <aeegarintawan5727@gmail.com>
2026-04-05 01:05:05 +0700

    aaa

--- users-app/src/app/api/departments/[id]/route.js
+++ users-app/src/app/api/departments/[id]/route.js
@@ -1,19 +1,25 @@
 import { NextResponse } from 'next/server';
 const pool = require('@/app/models/db_pool');
 
+// ดึงข้อมูลทั้งหมดมาโชว์ในตาราง
 export async function GET() {
-    const [rows] = await pool.query("SELECT * FROM department");
+    const [rows] = await pool.query("SELECT * FROM department ORDER BY department_id DESC");
     return NextResponse.json(rows);
 }
 
+// เพิ่มข้อมูลใหม่ (POST)
 export async function POST(req) {
-    const body = await req.json();
-    const { department_id, department_name } = body;
+    try {
+        const body = await req.json();
+        const { department_name, department_date } = body;
 
-    await pool.query(
-        "INSERT INTO department (department_id, department_name) VALUES (?, ?)",
-        [department_id, department_name]
-    );
+        await pool.query(
+            "INSERT INTO department (department_name, department_date) VALUES (?, ?)",
+            [department_name, department_date]
+        );
 
-    return NextResponse.json({ message: "เพิ่มสำเร็จ" })
+        return NextResponse.json({ message: "เพิ่มสำเร็จ" });
+    } catch (error) {
+        return NextResponse.json({ message: "Error", error: error.message }, { status: 500 });
+    }
 }
